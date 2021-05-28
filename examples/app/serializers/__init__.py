2dd4ce3a6b46f569a1b1c1fb6e7b27fd76f3c2e4
Ananto30 <azizulhaq.ananto@gmail.com>
2021-05-28 12:15:17 +0600

    Cleanup project

--- examples/app/serializers/__init__.py
+++
@@ -1,3 +0,0 @@
-from .serialization_types import Btype, Etype
-from .order_serializer import *
-from .auth_serializer import *
--- examples/app/serializers/order_serializer/__init__.py
+++
@@ -1,2 +0,0 @@
-from .serializer import CreateOrderReq, OrderResp, GetOrderReq
-from .rpc import OrderService, CreateOrder, GetOrder
