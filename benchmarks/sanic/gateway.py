f4147aba04318334b1393fa9420201dc0da642b5
Ananto30 <azizulhaq.ananto@gmail.com>
2021-05-28 11:36:55 +0600

    Fix sanic benchmark

--- benchmarks/sanic/gateway.py
+++ benchmarks/sanic/gateway.py
@@ -5,7 +5,7 @@ from sanic.response import text, json
 app = Sanic("My Hello, world app")
 
 
-@app.route('/')
+@app.route('/order')
 async def test(request):
     async with httpx.AsyncClient() as client:
         r = await client.post("http://localhost:8011/order", json={"user_id": "1", "items": ['apple', 'python']})
--- benchmarks/sanic/run_both.sh
+++ benchmarks/sanic/run_both.sh
@@ -1,3 +1,3 @@
 sanic benchmarks.sanic.gateway.app --workers=10 --port 8000 &
-sanic benchmarks.sanic.redis_reader.app --workers=10 --port 8011 &
+sanic benchmarks.sanic.sanic_server.app --workers=10 --port 8011 &
 wait
