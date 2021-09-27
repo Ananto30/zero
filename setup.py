812c53357df0591b7a90439201847e5f151f575f
ananto1 <azizulhaq.ananto@gmail.com>
2021-09-27 13:49:09 +0600

    Release force type hints

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeroapi",
-    version="0.0.2",
+    version="0.1.0",
     author="Azizul Haque Ananto",
     author_email="azizulhaq.ananto@gmail.com",
     license="MIT",
