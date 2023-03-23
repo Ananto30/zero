fac9cdb77719006b278365ff767ed9c40e7ff5ba
shimondoodkin <helpmepro1@gmail.com>
2023-03-23 18:08:04 +0800

    test threads, still not exiting stuck in socket.recv block, im considering using switching to socket.poll it works

--- tests/server_test.py
+++ tests/server_test.py
@@ -8,6 +8,8 @@ import signal
 import os
 import sys
 
+from zero.client import  ZeroClient
+
 async def echo(msg: str) -> str:
     return msg
 
@@ -23,8 +25,10 @@ def server2():
     app.register_rpc(echo)
     app.run()
 
+    
 
-def test_two_servers_can_be_run():
+
+def disabled_test_two_servers_can_be_run():
     try:
         p = Process(target=server1)
         p.start()
@@ -50,7 +54,7 @@ def test_two_servers_can_be_run():
 
 
 
-def test_server_run():
+def disabled_test_server_run():
 
     p = Process(target=server1)
     p.start()
@@ -58,3 +62,45 @@ def test_server_run():
     os.kill(p.pid,signal.CTRL_C_EVENT if sys.platform == 'win32' else signal.SIGINT)
     os.waitpid(p.pid ,0)
     # raise Exception() # uncomment see output
+
+
+
+def server3():
+    val={'val2':"top thread value"}
+    async def get(msg:str) -> str:
+       nonlocal val
+       return "got " + val['val2']
+
+    app = ZeroServer(port=4348, use_threads=True)
+    app.register_rpc(get)
+    app.run()
+    print("exit1")
+
+def client3():
+    zero_client = ZeroClient("127.0.0.1", 4348, default_timeout=100)
+    msg = zero_client.call("get",[""])
+    print(msg)
+    assert(msg=="got top thread value")
+    print("exit2")
+    # assert msg is None
+    
+def test_threads_server_run():
+    print("server3 starting")
+    p = Process(target=server3)
+    p.start()
+    time.sleep(5)
+    print("client3 starting")
+    p2 = Process(target=client3)
+    p2.start()
+
+    time.sleep(5) # 1 sec is too short nothing starts until 2 seconds
+    os.kill(p.pid,signal.CTRL_C_EVENT if sys.platform == 'win32' else signal.SIGINT)
+    
+    os.kill(p2.pid,signal.CTRL_C_EVENT if sys.platform == 'win32' else signal.SIGINT)
+    # os.waitpid(p.pid ,0)
+    # os.waitpid(p2.pid ,0)
+
+
+    # raise Exception() # uncomment see output
+
+# test_threads_server_run()
