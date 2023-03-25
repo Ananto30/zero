b3349fc9dd8cf34952d00ec76a20fc51eabe64b3
Ananto <azizulhaq.ananto@gmail.com>
2023-03-25 17:02:15 +0100

    Release v0.3.4-beta

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="zeroapi",
-    version="0.3.3",
+    version="0.3.4",
     author="Azizul Haque Ananto",
     author_email="azizulhaq.ananto@gmail.com",
     license="MIT",
