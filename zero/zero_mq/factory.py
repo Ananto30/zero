7e2e39518e0f22646d1b03d8036b98e9f88ce415
Ananto30 <azizulhaq.ananto@gmail.com>
2023-04-04 15:32:38 +0100

    fix: queue device name to proxy

--- zero/zero_mq/factory.py
+++ zero/zero_mq/factory.py
@@ -4,28 +4,28 @@ from .protocols import AsyncZeroMQClient, ZeroMQBroker, ZeroMQClient, ZeroMQWork
 
 
 def get_client(pattern: str, default_timeout: int = 2000) -> ZeroMQClient:
-    if pattern == "queue_device":
+    if pattern == "proxy":
         return queue_device.ZeroMQClient(default_timeout)
 
     raise ValueError(f"Invalid pattern: {pattern}")
 
 
 def get_async_client(pattern: str, default_timeout: int = 2000) -> AsyncZeroMQClient:
-    if pattern == "queue_device":
+    if pattern == "proxy":
         return queue_device.AsyncZeroMQClient(default_timeout)
 
     raise ValueError(f"Invalid pattern: {pattern}")
 
 
 def get_broker(pattern: str) -> ZeroMQBroker:
-    if pattern == "queue_device":
+    if pattern == "proxy":
         return queue_device.ZeroMQBroker()
 
     raise ValueError(f"Invalid pattern: {pattern}")
 
 
 def get_worker(pattern: str, worker_id: int) -> ZeroMQWorker:
-    if pattern == "queue_device":
+    if pattern == "proxy":
         return queue_device.ZeroMQWorker(worker_id)
 
     raise ValueError(f"Invalid pattern: {pattern}")
