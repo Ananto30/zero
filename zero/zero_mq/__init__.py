8ab74a1f7602543882c419adc68bda53cce5d14b
bswck <bswck.dev@gmail.com>
2023-04-07 01:21:33 +0200

    Move up # noqa: F401 for flake8 in __init__.py

--- zero/zero_mq/__init__.py
+++ zero/zero_mq/__init__.py
@@ -1,7 +1,7 @@
 from .factory import get_async_client, get_broker, get_client, get_worker  # noqa: F401
-from .protocols import (
+from .protocols import (  # noqa: F401
     AsyncZeroMQClient,
     ZeroMQBroker,
     ZeroMQClient,
     ZeroMQWorker,
-)  # noqa: F401
+)
